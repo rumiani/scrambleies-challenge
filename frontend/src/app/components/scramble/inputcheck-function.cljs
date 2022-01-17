15adf71e9e17639f6d98a5f94dfec885831b907b
maziar.rumiani <maziar.rumiani@gmail.com>
2022-01-17 09:09:04 +0330

    file name edit

--- frontend/src/app/components/scramble/inputcheck-function.cljs
+++
@@ -1,8 +0,0 @@
-(ns app.components.scramble.inputcheck-function
-  (:require 
-      [goog.string :as gstring]
-))
-
-(defn inputcheck-function [string]
-   (= (re-find #"[a-z]+" string) (gstring/trim string)))
-
---
+++ frontend/src/app/components/scramble/inputcheck_function.cljs
@@ -0,0 +1,7 @@
+(ns app.components.scramble.inputcheck-function
+  (:require
+   [goog.string :as gstring]))
+
+(defn inputcheck-function [string]
+  (= (re-find #"[a-z]+" string) (gstring/trim string)))
+
