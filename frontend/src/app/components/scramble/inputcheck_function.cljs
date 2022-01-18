536930f95691a244c250f361e229bf07e8bd1fe6
maziar.rumiani <maziar.rumiani@gmail.com>
2022-01-18 06:11:31 +0330

    a small edit in inputcheck_function

--- frontend/src/app/components/scramble/inputcheck_function.cljs
+++ frontend/src/app/components/scramble/inputcheck_function.cljs
@@ -1,7 +1,7 @@
-(ns app.components.scramble.inputcheck-function
+(ns app.components.scramble.inputcheck_function
   (:require
    [goog.string :as gstring]))
 
-(defn inputcheck-function [string]
+(defn inputcheck_function [string]
   (= (re-find #"[a-z]+" string) (gstring/trim string)))
 
